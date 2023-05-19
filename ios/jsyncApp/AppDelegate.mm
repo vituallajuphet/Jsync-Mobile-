556d7ce49cbbd8adcea44a05dc2eea4f7668cebd
Juphet Vitualla <jvitualla@dnamicro.com>
2023-05-19 13:42:43 +0800

    update pod

--- ios/jsyncApp/AppDelegate.mm
+++ ios/jsyncApp/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -7,9 +8,13 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"jsyncApp";
+
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  if([FIRApp defaultApp] == nil){
+    [FIRApp configure];
+  }
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
